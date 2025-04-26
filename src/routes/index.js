ca0180fbd697913381ebfe8585763964a336a415
MinhUy9999 <154686164+MinhUy9999@users.noreply.github.com>
2025-04-26 11:13:10 +0700

    commit login register

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const { register, login } = require("../controllers/user.controller");
 const router = express.Router();
 
 // Route chính
 router.get("/", (req, res) => {
+
   res.json({ message: "Welcome to Booking API" });
 });
-
+router.post("/api/v1/register", register);
+router.post("/api/v1/login", login);
 // Thêm các routes khác ở đây
 // router.use('/api/bookings', require('./booking.routes'));
 // router.use('/api/users', require('./user.routes'));
